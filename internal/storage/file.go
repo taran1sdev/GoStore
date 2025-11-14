063065ac9df0e2e8f5a110bb89a38cc996432eaa
taran1s <taran1sdev@gmail.com>
2025-11-14 12:10:20 +0000

    Implemented functions to append records to DB file

--- internal/storage/file.go
+++ internal/storage/file.go
@@ -75,7 +75,11 @@ func checkSignature(f *os.File) (bool, error) {
 	return true, nil
 }
 
-var ERROR_CORRUPT_RECORD = fmt.Errorf("Encountered a Corrupt Record")
+var (
+	ErrCorruptRecord = fmt.Errorf("Encountered a corrupt record")
+	ErrKeyTooLarge   = fmt.Errorf("Maximum key length is 1024 bytes")
+	ErrValTooLarge   = fmt.Errorf("Maximum value length is 32768 bytes")
+)
 
 const (
 	maxKeyLength uint16 = 1024
@@ -93,14 +97,14 @@ func parseRecord(f *os.File) (*Record, error) {
 	if errors.Is(kErr, io.EOF) && kRead == 0 {
 		return nil, io.EOF
 	} else if errors.Is(kErr, io.ErrUnexpectedEOF) {
-		return nil, ERROR_CORRUPT_RECORD
+		return nil, ErrCorruptRecord
 	} else if kErr != nil {
 		return nil, fmt.Errorf("Encountered an error during record parsing: %w", kErr)
 	}
 
 	_, vErr := io.ReadFull(f, valLenRaw)
 	if vErr != nil {
-		return nil, ERROR_CORRUPT_RECORD
+		return nil, ErrCorruptRecord
 	}
 
 	// Convert the values
@@ -109,14 +113,14 @@ func parseRecord(f *os.File) (*Record, error) {
 
 	// Make sure key and value lengths are within bounds
 	if keyLen > maxKeyLength || valLen > maxValLength {
-		return nil, ERROR_CORRUPT_RECORD
+		return nil, ErrCorruptRecord
 	}
 
 	flagRaw := make([]byte, 1)
 	// Parse and convert flags
 	_, fErr := io.ReadFull(f, flagRaw)
 	if fErr != nil {
-		return nil, ERROR_CORRUPT_RECORD
+		return nil, ErrCorruptRecord
 	}
 
 	flag := Flag(flagRaw[0])
@@ -125,13 +129,13 @@ func parseRecord(f *os.File) (*Record, error) {
 	key := make([]byte, keyLen)
 	_, keyErr := io.ReadFull(f, key)
 	if keyErr != nil {
-		return nil, ERROR_CORRUPT_RECORD
+		return nil, ErrCorruptRecord
 	}
 
 	value := make([]byte, valLen)
 	_, valErr := io.ReadFull(f, value)
 	if valErr != nil {
-		return nil, ERROR_CORRUPT_RECORD
+		return nil, ErrCorruptRecord
 	}
 
 	return &Record{
@@ -141,6 +145,8 @@ func parseRecord(f *os.File) (*Record, error) {
 	}, nil
 }
 
+// Currently our database is actually a log of operations - this functions replays those operations
+// and loads the result into memory
 func (s *Storage) Replay() ([]*Record, error) {
 	var records []*Record
 
@@ -162,4 +168,115 @@ func (s *Storage) Replay() ([]*Record, error) {
 	pos, _ := s.File.Seek(0, io.SeekCurrent)
 	s.writeOffset = pos
 	return records, nil
+}
+
+// I repeat myself with these functions but im not too worried about optimization at this stage
+
+func (s *Storage) AppendSet(key, value []byte) error {
+	// Check field lengths are withing parameters
+	if len(key) > maxKeyLength {
+		return ErrKeyTooLarge
+	} else if len(value) > maxValLength {
+		return ErrValTooLarge
+	}
+
+	r := &Record{
+		Key:   key,
+		Value: value,
+		Flag:  FlagSet,
+	}
+
+	err := s.writeRecord(r)
+
+	if err != nil {
+		return fmt.Errorf("Failed to write record: %w", err)
+	}
+	return nil
+}
+
+func (s *Storage) AppendDelete(key []byte) error {
+	// Make sure it's a valid key
+	if len(key) > maxKeyLength {
+		return ErrKeyTooLarge
+	}
+
+	r := &Record{
+		Key:   key,
+		Value: nil,
+		Flag:  FlagDel,
+	}
+
+	err := s.writeRecord(r)
+
+	if err != nil {
+		return fmt.Errorf("Failed to write record: %w", err)
+	}
+	return nil
+}
+
+// This function appends records to our operation log
+func (s *Storage) writeRecord(r *Record) error {
+
+	// Convert our values correctly
+	keyLen, valLen := uint16(len(r.Key)), uint16(len(r.Value))
+
+	// the info fields will always be 5 bytes then add the length of the data
+	recordLen := 5 + len(r.Key) + len(r.Value)
+
+	var err error
+
+	// We use a closure to avoid verbosity
+	write := func(n int, e error) {
+		if err == nil && e != nil {
+			err = e
+		}
+	}
+
+	writeByte := func(e error) {
+		if err == nil && e != nil {
+			err = e
+		}
+	}
+
+	// Using a buffer we only need to write to the file once and check for errors less
+	buf := bytes.NewBuffer(make([]byte, 0, recordLen))
+
+	var keySlice = [2]byte
+	var valSlice = [2]byte
+
+	binary.LittleEndian.PutUint16(keySlice[:], keyLen)
+	write(buf.Write(keySlice[:]))
+
+	binary.LittleEndian.PutUint16(valSlice[:], valLen)
+	write(buf.Write(valSlice[:]))
+
+	writeByte(buf.WriteByte(byte(r.Flag)))
+
+	write(buf.Write(r.Key))
+
+	if r.Flag == FlagSet {
+		write(buf.Write(r.Value))
+	}
+
+	if err != nil {
+		return fmt.Errorf("Encountered an error writing data to buffer: %w", err)
+	}
+
+	// Make sure our pointer is at EOF
+	if _, seekErr := s.File.Seek(0, io.SeekEnd); seekErr != nil {
+		return fmt.Errorf("Failed to seek to end of file: %w", seekErr)
+	}
+
+	// Write the data in the buffer to the file
+	l, writeErr := s.File.Write(buf.Bytes())
+	if writeErr != nil {
+		err = fmt.Errorf("Encountered an error while writing to DB file: %w", writeErr)
+	} else if l != recordLen {
+		err = fmt.Errorf("Bytes written mismatch: Expected: %d Actual: %d", recordLen, l)
+	}
+
+	// Always reset the write offset even when encountering errors
+	s.writeOffset = s.File.Seek(0, io.SeekCurrent)
+	// err should be null if everything worked
+	return err
 }
