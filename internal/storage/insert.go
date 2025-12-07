1ace87371fa0cba3cbed4cc98fe4596ba9513c2f
taran1s <taran1sdev@gmail.com>
2025-12-07 13:19:00 +0000

    Refactored Insert function for modularity and readability

--- internal/storage/insert.go
+++ internal/storage/insert.go
@@ -5,108 +5,126 @@ import (
 	"errors"
 )
 
-func (bt *BTree) Insert(key, val []byte) (bool, error) {
+func (bt *BTree) descendForInsert(key []byte) (*LeafPage, *ParentStack, error) {
 	curr := bt.root
 	stack := &ParentStack{}
 
-	var sepKey []byte
-	var rightPageID uint32
-	propagating := false
+	for {
+		page, err := bt.pager.ReadPage(curr)
+		if err != nil {
+			return nil, nil, err
+		}
+
+		switch page.Type {
+		case PageTypeLeaf:
+			return WrapLeafPage(page), stack, nil
+
+		case PageTypeInternal:
+			internal := WrapInternalPage(page)
+			idx := internal.FindInsertIndex(key)
+
+			stack.Push(Parent{pageID: curr})
+
+			if idx < internal.GetNumKeys() {
+				curr = internal.GetChild(idx)
+			} else {
+				curr = internal.GetRightChild()
+			}
+		}
+	}
+}
+
+func (bt *BTree) insertIntoLeaf(leaf *LeafPage, key, val []byte) (bool, []byte, uint32, error) {
+	// First try and insert the key, val into the leafpage
+	if err := leaf.Insert(key, val); err == nil {
+		return true, nil, 0, bt.pager.WritePage(leaf.Page)
+	} else if errors.Is(err, ErrKeyExists) {
+		return false, nil, 0, err
+	}
+
+	// If we get any other error it means the page is full and we have to split
+	sepKey, rightPageID := bt.splitLeaf(leaf)
+
+	// Now decide which leaf to insert the value into after the split
+	var err error
+	if bytes.Compare(key, sepKey) <= 0 {
+		// There is always space after a split
+		_ = leaf.Insert(key, val)
+		err = bt.pager.WritePage(leaf.Page)
+	} else {
+		right, _ := bt.pager.ReadPage(rightPageID)
+		rleaf := WrapLeafPage(right)
+		_ = rleaf.Insert(key, val)
+		err = bt.pager.WritePage(rleaf.Page)
+	}
+
+	return false, sepKey, rightPageID, err
+}
+
+func (bt *BTree) propogateSplit(
+	parentStack *ParentStack,
+	sepKey []byte,
+	leftID,
+	rightID uint32,
+) (bool, error) {
 
 	for {
-		// Read the current page
-		page, err := bt.pager.ReadPage(curr)
+		// Try pop our previously visited node
+		parent, ok := parentStack.Pop()
+
+		// if no parent then we need to create a new root node
+		if !ok {
+			return bt.growRoot(sepKey, leftID, rightID)
+		}
+
+		page, err := bt.pager.ReadPage(parent.pageID)
 		if err != nil {
 			return false, err
 		}
 
-		if !propagating {
-			// Once we hit a leaf node try and insert the record
-			if page.Type == PageTypeLeaf {
-				leafPage := WrapLeafPage(page)
-				if fErr := leafPage.Insert(key, val); fErr == nil {
-					return true, bt.pager.WritePage(leafPage.Page)
-				} else if errors.Is(fErr, ErrKeyExists) {
-					return false, fErr
-				}
-				// If the page is full trigger a split
-				sepKey, rightPageID = bt.splitLeaf(leafPage)
+		internal := WrapInternalPage(page)
 
-				// Compare the key to be inserted with the seperator key
-				// and insert into the correct leaf page
-				if bytes.Compare(key, sepKey) <= 0 {
-					_ = leafPage.Insert(key, val)
-					bt.pager.WritePage(leafPage.Page)
-				} else {
-					right, _ := bt.pager.ReadPage(rightPageID)
-					rightLeaf := WrapLeafPage(right)
-					_ = rightLeaf.Insert(key, val)
-					bt.pager.WritePage(rightLeaf.Page)
-				}
-				// No parent means we need to create a new root node
-				parent, ok := stack.Pop()
-				if !ok {
-					return bt.growRoot(sepKey, leafPage.Page.ID, rightPageID)
-				}
-
-				// Start propagating up the tree
-				curr = parent.pageID
-				propagating = true
-				continue
-
-				// Traverse the internal pages
-			} else if page.Type == PageTypeInternal {
-				internalPage := WrapInternalPage(page)
-				idx := internalPage.FindInsertIndex(key)
-				stack.Push(Parent{
-					pageID: curr,
-				})
-				if idx < internalPage.GetNumKeys() {
-					curr = internalPage.GetChild(idx)
-				} else {
-					curr = internalPage.GetRightChild()
-				}
-			}
-			// Propagation after a split
-		} else {
-			internalPage := WrapInternalPage(page)
-			// We successfully updated internal pages so we can return
-			if !internalPage.InsertSeparator(sepKey, rightPageID) {
-				return true, bt.pager.WritePage(internalPage.Page)
-			}
-
-			origKey, origChild := sepKey, rightPageID
-			// No space for key means we need to split the internal page
-			sepKey, rightPageID = bt.splitInternal(internalPage)
-
-			left := internalPage
-			r, err := bt.pager.ReadPage(rightPageID)
-			if err != nil {
-				return false, err
-			}
-
-			right := WrapInternalPage(r)
-
-			if bytes.Compare(origKey, sepKey) < 0 {
-				if !left.InsertSeparator(origKey, origChild) {
-					if err := bt.pager.WritePage(left.Page); err != nil {
-						return false, err
-					}
-				}
-			} else {
-				if !right.InsertSeparator(origKey, origChild) {
-					if err := bt.pager.WritePage(right.Page); err != nil {
-						return false, err
-					}
-				}
-			}
-
-			parent, ok := stack.Pop()
-			if !ok {
-				return bt.growRoot(sepKey, internalPage.Page.ID, rightPageID)
-			}
-
-			curr = parent.pageID
+		// Try insert the new separator into the parent (false means no split required)
+		if !internal.InsertSeparator(sepKey, rightID) {
+			return true, bt.pager.WritePage(internal.Page)
 		}
+
+		// If we need an internal split - keep track of the original separator to insert
+		origKey, origChild := sepKey, rightID
+
+		sepKey, rightID = bt.splitInternal(internal)
+
+		leftNode := internal
+		rightPage, _ := bt.pager.ReadPage(rightID)
+		rightNode := WrapInternalPage(rightPage)
+
+		if bytes.Compare(origKey, sepKey) < 0 {
+			_ = leftNode.InsertSeparator(origKey, origChild)
+			bt.pager.WritePage(leftNode.Page)
+		} else {
+			_ = rightNode.InsertSeparator(origKey, origChild)
+			bt.pager.WritePage(rightNode.Page)
+		}
+
+		leftID = leftNode.Page.ID
 	}
+}
+
+// Entry point into insertion logic
+func (bt *BTree) Insert(key, val []byte) (bool, error) {
+	leaf, parentStack, err := bt.descendForInsert(key)
+	if err != nil {
+		return false, err
+	}
+
+	inserted, sepKey, rightPageID, err := bt.insertIntoLeaf(leaf, key, val)
+	if err != nil {
+		return false, err
+	}
+
+	if inserted {
+		return true, nil
+	}
+
+	return bt.propogateSplit(parentStack, sepKey, leaf.Page.ID, rightPageID)
 }
